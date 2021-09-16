b285cd08a7c4089e20ed30c02eb632a31f68ff44
Daniel <danielz.nas@gmail.com>
2021-09-16 12:53:26 -0300

    Got the match() function to work and keep track of player and computer points. added a while loop to end match once either reach 5 points

---
+++ script.js
@@ -0,0 +1,71 @@
+function round() {
+
+    let userInput = prompt('Choose: Rock Paper or Scissor');
+    let userChoice = userInput.toLowerCase();
+    let compChoice = rdmValue();
+    
+    function rdmValue() {
+        let cmpSelection = Math.random();
+        
+        if (cmpSelection < 0.33) {
+            return 'rock';
+            } else if (cmpSelection >= 0.33 && cmpSelection <= 0.66) {
+            return 'paper';
+            } else {
+            return 'scissor';
+        }
+    }
+    
+    function compare(choice1, choice2) {
+    
+        if (choice1 === choice2) {
+        console.log('This round is A TIE!');
+        return 0;
+        } else if (choice1 === 'rock' && choice2 === 'paper') {
+        console.log('Paper beats Rock, YOU LOOSE this round!');
+        return 1;
+        } else if (choice1 === 'rock' && choice2 === 'scissor') {
+        console.log('Rock beats Scissors, YOU WIN this round!');
+        return 2;
+        } else if (choice1 === 'paper' && choice2 === 'rock') {
+        console.log('Paper beats Rock, YOU WIN this round!');
+        return 2;
+        } else if (choice1 === 'paper' && choice2 === 'scissor') {
+        console.log('Scissor beats Paper, YOU LOOSE this round!');
+        return 1;
+        } else if (choice1 === 'scissor' && choice2 === 'rock') {
+        console.log('Rock beats Scissors, YOU LOOSE this round!');
+        return 1;
+        } else if (choice1 === 'scissor' && choice2 === 'paper') {
+        console.log('Scissor beats Paper, YOU WIN this round!');
+        return 2;
+        }
+    }
+    return compare(userChoice, compChoice);
+    
+    }
+    
+    
+    function match(){
+    
+        let playerPoints = 0;
+        let comptPoints = 0;
+
+        while (playerPoints < 5 && comptPoints < 5) {
+            let runRound = round();
+            if (runRound === 0) {
+            continue;
+            } else if (runRound === 1) {
+            comptPoints++;
+            } else if (runRound === 2) {
+            playerPoints++;
+            }
+            console.log("player: " + playerPoints);
+            console.log("computer: " + comptPoints);
+        }
+        if (playerPoints === 5) {
+        console.log("you won the match");
+        } else if (comptPoints === 5) {
+        console.log("you lost the match");
+        }
+    };    
